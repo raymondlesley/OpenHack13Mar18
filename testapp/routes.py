b3966b00fed3b82a4ea94aeab6742965749a4164
raymondlesley <git.raymond@thelesleys.co.uk>
2018-03-14 13:27:24 +0000

    incorporate Challenge 3 image classification

--- testapp/routes.py
+++ testapp/routes.py
@@ -1,10 +1,19 @@
 from testapp import app
 
+global_data = ""
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+	return "Hello, World!"
+	global_data = "initialised"
 	
 @app.route('/test')
 def test():
-	return "Test OK!"
+	return "Test OK!" + global_data
+
+from testapp import trainer
+@app.route('/train')
+def train():
+	output = trainer.trainer()
+	return "<pre>" + output + "</pre>"
